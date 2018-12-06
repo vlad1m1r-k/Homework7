dca6459cd7ecae0cf4a54b54ca1f3a0d0e3964eb
Vladimir <vova.kvz3@gmail.com>
2018-12-06 21:35:58 +0200

    ""

--- src/ua/kiev/prog/homework7/part3/SearchFile.java
+++ src/ua/kiev/prog/homework7/part3/SearchFile.java
@@ -1,11 +1,16 @@
 package ua.kiev.prog.homework7.part3;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SearchFile {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         if (args.length < 2) {
             System.out.println("Usage: SearchFile StartDirectory FileName");
         } else {
@@ -14,8 +19,22 @@ public class SearchFile {
             if (!startDir.isDirectory()) {
                 System.out.println("Wrong start point.");
             } else {
-                ExecutorService eService = Executors.newFixedThreadPool(10);
-                //TODO Start Tasks
+                ExecutorService eService = Executors.newFixedThreadPool(30);
+                List<Future<?>> list = new ArrayList<>();
+                list.add(eService.submit(new SearchThread(eService, startDir, fileName, list)));
+                for (Future<?> future : list) {
+                        future.get();
+                }
+                boolean finish = true;
+                while (finish) {
+                    for (Future<?> future : list) {
+                        if (!future.isDone()) {
+                            future.get();
+                            break;
+                        }
+                        finish = false;
+                    }
+                }
                 eService.shutdown();
             }
         }
--- src/ua/kiev/prog/homework7/part3/SearchThread.java
+++ src/ua/kiev/prog/homework7/part3/SearchThread.java
@@ -1,11 +1,34 @@
 package ua.kiev.prog.homework7.part3;
 
-import java.util.concurrent.Callable;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 public class SearchThread implements Runnable {
+    private ExecutorService executor;
+    private File startDir;
+    private String fileName;
+    private List<Future<?>> list;
+
+    public SearchThread(ExecutorService executor, File startDir, String fileName, List<Future<?>> list) {
+        this.executor = executor;
+        this.startDir = startDir;
+        this.fileName = fileName;
+        this.list = list;
+    }
 
     @Override
     public void run() {
-
+        File[] files = startDir.listFiles();
+        for (File file : files) {
+            if (!file.isDirectory() && file.getName().equals(fileName)) {
+                System.out.println(file.getAbsolutePath());
+            }
+            if (file.isDirectory() && !Files.isSymbolicLink(file.toPath())) {
+                list.add(executor.submit(new SearchThread(executor, file, fileName, list)));
+            }
+        }
     }
 }
